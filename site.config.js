1d9ef39712994b847ea273d09d38bd456825ce6b
mahir-41 <mahiralam41@gmail.com>
2023-02-06 11:16:25 +0600

    first commit

---
+++ site.config.js
@@ -0,0 +1,9 @@
+//const projects = require('./src/data/projects')
+
+module.exports = {
+    site: {
+      title: 'My Static Site',
+      description: 'Micro Static Site Generator in Node.js',
+      //projects,
+    },
+  }
